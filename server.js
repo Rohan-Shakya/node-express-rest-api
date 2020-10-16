691f063aba53956bce065faa8243dbff9bee2d9c
Rohan <rohanshakya254@gmail.com>
2020-10-16 20:17:21 +0545

    added default route

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const { getProducts } = require('./controllers/productController');
 const server = http.createServer((req, res) => {
   if (req.url === '/api/products' && req.method === 'GET') {
     getProducts(req, res);
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Route not found' }));
   }
 });
 
