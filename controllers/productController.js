02b982ffe3b2748dbf017f68bd2da9d8b9a20ecd
Rohan <rohanshakya254@gmail.com>
2020-10-16 20:26:43 +0545

    added GET single request

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,7 +5,6 @@ const Product = require('../models/productModel');
 const getProducts = async (req, res) => {
   try {
     const products = await Product.findAll();
-
     res.writeHead(200, { 'Content-Type': 'application/json' }); // res.statusCode() + res.setHeader()
     res.end(JSON.stringify(products)); // res.write() + res.end()
   } catch (err) {
@@ -13,6 +12,24 @@ const getProducts = async (req, res) => {
   }
 };
 
+// Gets Single Product
+// GET /api/product/:id
+async function getProduct(req, res, id) {
+  try {
+    const product = await Product.findById(id);
+    if (!product) {
+      res.writeHead(404, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: 'Product Not Found' }));
+    } else {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(product));
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 module.exports = {
   getProducts,
+  getProduct,
 };
