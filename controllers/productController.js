c71c52cf686ab1fc70af47bb453e547613e857cc
Rohan <rohanshakya254@gmail.com>
2020-10-16 20:36:22 +0545

    added PUT method

--- controllers/productController.js
+++ controllers/productController.js
@@ -51,8 +51,39 @@ async function createProduct(req, res) {
   }
 }
 
+// Update a Product
+// PUT /api/product/:id
+async function updateProduct(req, res, id) {
+  try {
+    const product = await Product.findById(id);
+
+    if (!product) {
+      res.writeHead(404, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: 'Product Not Found' }));
+    } else {
+      const body = await getPostData(req);
+
+      const { title, description, price } = JSON.parse(body);
+
+      const productData = {
+        title: title || product.title,
+        description: description || product.description,
+        price: price || product.price,
+      };
+
+      const updProduct = await Product.update(id, productData);
+
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      return res.end(JSON.stringify(updProduct));
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 module.exports = {
   getProducts,
   getProduct,
   createProduct,
+  updateProduct,
 };
